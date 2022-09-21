8e82af665ab80e670dd2c69f3348c4c4c2260324
YasashiisGod <54971293+yasashiisgod@users.noreply.github.com>
2022-09-20 20:21:41 -0600

    Initial Layout of the project

--- run-flask.sh
+++ run-flask.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export FLASK_APP=wiki
+export FLASK_APP=run.py
 export FLASK_ENV=development
 flask run -p 54939
---
+++ run.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+from web import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
