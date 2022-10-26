ec08e0b11de1965abd7b1f885fc27f7cbf5cf455
Kevin M <kjmoreno2@miners.utep.edu>
2022-10-25 20:34:15 -0600

    added the files for the different containers, these files are meant to hold the functionalities that the system is required to have

---
+++ web/canbuscontroller.py
@@ -0,0 +1,20 @@
+# add all functionality of the can bus controller try to make connection and such
+#
+#
+#
+#
+#
+
+#class packethandler:
+#handle the traffic actions such as reading writing
+#
+#class connectionverifier:
+    #def canconnect(self):
+        #try:
+        #if (bus = connectionfunctioncall()):
+        #   return boolean value
+        #except:
+
+#if (connectionverifier.canconnect()):
+
+
